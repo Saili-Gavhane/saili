06c75b8912e0b449fb5407b603ac1a8979df4625
Saili-Gavhane <57283389+Saili-Gavhane@users.noreply.github.com>
2019-11-02 04:30:04 -0700

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -5,6 +5,6 @@ public static void main(String args[])
 System.out.println("Welcome to git this is master");
 System.out.println("this is tom");
 System.out.println("this is simrit");
-  System.out.println("this is sim");
+  System.out.println("this is branch1");
 }
 }
